d49a6faee4ac45b09a4d915e853c0d8adf3945fd
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2021-10-21 16:43:11 +0300

    Create libvirt formula

---
+++ Formula/libvirt.rb
@@ -0,0 +1,61 @@
+class Libvirt < Formula
+  desc "C virtualization API"
+  homepage "https://github.com/shchuko/libvirt"
+  url "https://github.com/shchuko/libvirt/releases/download/v7.8.0/libvirt-v7.8.0.tar.gz"
+  version "7.8.0"
+  sha256 "4af9338e64f817d0ce6f0b8a24a9e2f72ae7ff787a66afe54ea37fa9e448655d"
+  license all_of: ["LGPL-2.1-or-later", "GPL-2.0-or-later"]
+  head "https://github.com/shchuko/libvirt.git", branch: "master"
+
+  depends_on "docutils" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "perl" => :build
+  depends_on "pkg-config" => :build
+  depends_on "python@3.9" => :build
+  depends_on "rpcgen" => :build
+  depends_on "gettext"
+  depends_on "glib"
+  depends_on "gnu-sed"
+  depends_on "gnutls"
+  depends_on "grep"
+  depends_on "libgcrypt"
+  depends_on "libiscsi"
+  depends_on "libssh2"
+  depends_on "yajl"
+
+  uses_from_macos "curl"
+  uses_from_macos "libxslt"
+
+  def install
+    mkdir "build" do
+      args = %W[
+        --localstatedir=#{var}
+        --mandir=#{man}
+        --sysconfdir=#{etc}
+        -Ddriver_esx=enabled
+        -Ddriver_qemu=enabled
+        -Dinit_script=none
+      ]
+      system "meson", *std_meson_args, *args, ".."
+      system "meson", "compile"
+      system "meson", "install"
+    end
+  end
+
+  service do
+    run [opt_sbin/"libvirtd", "-f", etc/"libvirt/libvirtd.conf"]
+    keep_alive true
+    environment_variables PATH: HOMEBREW_PREFIX/"bin"
+  end
+
+  test do
+    if build.head?
+      output = shell_output("#{bin}/virsh -V")
+      assert_match "Compiled with support for:", output
+    else
+      output = shell_output("#{bin}/virsh -v")
+      assert_match version.to_s, output
+    end
+  end
+end
