cbf90b0eb4448bbb80bfeee6d636d89561146310
Vladislav Yaroshchuk <Vladislav.Yaroshchuk@jetbrains.com>
2022-03-17 00:00:27 +0300

    [qemu] drop unused floppy image

--- Formula/qemu.rb
+++ Formula/qemu.rb
@@ -27,12 +27,6 @@ class Qemu < Formula
 
   fails_with gcc: "5"
 
-  # 820KB floppy disk image file of FreeDOS 1.2, used to test QEMU
-  resource "test-image" do
-    url "https://www.ibiblio.org/pub/micro/pc-stuff/freedos/files/distributions/1.2/FD12FLOPPY.zip"
-    sha256 "81237c7b42dc0ffc8b32a2f5734e3480a3f9a470c50c14a9c4576a2561a35807"
-  end
-
   def install
     ENV["LIBTOOL"] = "glibtool"
 
